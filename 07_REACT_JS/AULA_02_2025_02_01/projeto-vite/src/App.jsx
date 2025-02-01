807302710bb35fa4c79a8ea9d643f69d4e42a28c
Márcio Ferreira <hmarciogferreira@gmail.com>
2025-02-01 17:24:59 -0300

    Ajustes

--- 07_REACT_JS/AULA_02_2025_02_01/projeto-vite/src/App.jsx
+++ 07_REACT_JS/AULA_02_2025_02_01/projeto-vite/src/App.jsx
@@ -5,12 +5,21 @@ function App() {
   
   // CADA COMPONENTE TEM SEU ESTADOS.
   const [contador, setContador] = useState(1)
+  const [tema, setTema] = useState('light')
   
   function aumentar() {
     setContador(contador + 1)
   }
   function diminuir() {
     setContador(contador - 1)
+  }
+
+  function trocarTema() {
+    if(tema === 'light') {
+      setTema('dark')
+    } else {
+      setTema('light')
+    }
   }
 
   useEffect(() => {
@@ -20,12 +29,16 @@ function App() {
   }, [contador])
 
   return (
-    <>
+    <div id={tema}>
+      {tema}
+      <button onClick={() => trocarTema()}>
+        {tema === 'light' ? 'Escuro' : 'Claro'}
+      </button>
       <h1>FDEV009 - {contador}</h1>
       <button onClick={aumentar}>Aumentar</button>
       <button onClick={diminuir}>Diminuir</button>
       <Header />
-    </>
+    </div>
   )
 }
 
